d3764c6e48bf6ae67dc43e6398ab7ebe88b97926
Nicholas <mykolakratasyk@gmail.com>
2023-06-13 17:18:22 +0300

    rabotaem

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
+	// ДЛЯ ВСЕХ(!!!!!!) ВСПЛЫВАЮЩИХ ОКОН [Если кликаем за пределы всплывающего окна, закрываем это окно]
+	$("html").on("click", function (e) {
+		if (
+			!$(e.target).closest(".header__burger").length &&
+            !$(e.target).closest(".nav__mobile-close").length &&
+			!$(e.target).closest("#nav-mobile").length
+		) {
+			$("body").removeClass("overflow-hidden");
+			$("#page").removeClass("bg-overlay");
 
-   
-})
+			$("#nav-mobile").removeClass("active");
+			$("#nav").removeClass("active");
+		}
+	});
+
+	$(".header__burger").on("click", function () {
+		$(this).toggleClass("active");
+
+		$("#nav-mobile").toggleClass("active");
+
+		$("body").toggleClass("overflow-hidden");
+		$("#page").toggleClass("bg-overlay");
+	});
+
+    $(".nav__mobile-close").on("click", function () {
+		$(this).toggleClass("active");
+
+		$("#nav-mobile").toggleClass("active");
+
+		$("body").toggleClass("overflow-hidden");
+		$("#page").toggleClass("bg-overlay");
+	});
+
+	// $('#nav .btn-style').on('click', function() {
+	//     $("body").removeClass("overflow-hidden")
+	//     $('#page').removeClass('bg-overlay')
+
+	//     $('#nav').removeClass('active')
+	//     $('#menu').removeClass('active')
+	// })
+
+	// $('#popup-success .btn-style').on('click', function() {
+	//     $('.is-close').trigger('click')
+	// })
+});
